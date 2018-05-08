560014bba27234f1c495f07dd82faded525f927e
Egar Garcia <egar.garcia@gmail.com>
2018-05-08 01:55:02 -0700

    Progress on 2018-05-08 01:54

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -9,20 +9,18 @@ using json = nlohmann::json;
 
 
 PathPlanner::PathPlanner(
-    const double &max_speed_mph,
-    const double &move_time,
-    const double &max_acceleration,
-    const double &security_seconds_ahead,
-    const double &planning_seconds_ahead,
-    const double &takeover_agressivity_rate,
-    const double &action_seconds) {
+    const double &max_speed_mph, const double &move_time, const double &max_acceleration,
+    const double &security_seconds_ahead, const double &planning_seconds_ahead,
+    const double &action_seconds, const double &takeover_agressivity_rate,
+    const double &security_reaction_rate) {
   this -> max_speed_mph = max_speed_mph;
   this -> move_time = move_time;
   this -> max_acceleration = max_acceleration;
   this -> security_seconds_ahead = security_seconds_ahead;
   this -> planning_seconds_ahead = planning_seconds_ahead;
-  this -> takeover_agressivity_rate = takeover_agressivity_rate;
   this -> action_seconds = action_seconds;
+  this -> takeover_agressivity_rate = takeover_agressivity_rate;
+  this -> security_reaction_rate = security_reaction_rate;
   this -> ideal_speed = tools.mph2ms(max_speed_mph - 1);
   this -> ideal_acceleration = max_acceleration / 2.0;
   this -> no_next_vals = round(planning_seconds_ahead / move_time);
@@ -40,8 +38,6 @@ vector<vector<double>> PathPlanner::updatePath(
   }
 
   const vector<double> current_pos = getCurrentPosition(car_data);
-  //double current_speed = current_pos[3];
-  //double current_pos_s = current_pos[4];
 
   // Previous path data given to the Planner
   vector<double> previous_path_x = car_data["previous_path_x"];
@@ -50,36 +46,30 @@ vector<vector<double>> PathPlanner::updatePath(
 
   vector<double> planned_pos = getPlannedPosition(current_pos, previous_path_x, previous_path_y,
                                                   map_waypoints_x, map_waypoints_y);
-  //double planned_speed = planned_pos[SPEED_IDX];
-  //double planned_pos_s = planned_pos[S_IDX];
 
   // Sensor Fusion Data, a list of all other cars on the same side of the road.
-  const vector<vector<double>> sensor_fusion = car_data["sensor_fusion"];
+  vector<vector<double>> sensor_fusion = car_data["sensor_fusion"];
   const ScanStatus scan_status = scanSurroundings(current_pos, prev_path_size * move_time,
                                                   planned_pos, sensor_fusion);
   cout << "--> " << "LANE: " << lane
        << ", carInFrontIsTooClose: " << scan_status.carInFrontIsTooClose
-       << ", carInFrontIsDangerouslyClose: " << scan_status.carInFrontIsDangerouslyClose
        << ", gapAtLeftLane: " << scan_status.gapAtLeftLane
        << ", gapAtRightLane: " << scan_status.gapAtRightLane
+       << ", carAheadAtLeftLane: " << (scan_status.carAheadAtLeftLane != NULL ? scan_status.carAheadAtLeftLane->at(SENSOR_FUSION_S_IDX) : -99999.999)
+       << ", carAheadInFront: " << (scan_status.carAheadInFront != NULL ? scan_status.carAheadInFront->at(SENSOR_FUSION_S_IDX) : -99999.999)
+       << ", carAheadAtRightLane: " << (scan_status.carAheadAtRightLane != NULL ? scan_status.carAheadAtRightLane->at(SENSOR_FUSION_S_IDX) : -99999.999)
        << endl;
 
   const State state = getNextState(scan_status);
-  /*
-  if (state == EMERGENCY_BREAK) {
-    planned_pos = current_pos;
-    ptr_previous_path_x = NULL;
-    ptr_previous_path_y = NULL;
-    prev_path_size = 0;
-  } else
-  */
+
   if (state == CHANGE_TO_LEFT_LANE) {
     lane--;
   } else if (state == CHANGE_TO_RIGHT_LANE) {
     lane++;
   }
 
-  vector<vector<double>> spline_pts = createPathPoints(current_pos, previous_path_x, previous_path_y);
+  vector<vector<double>> spline_pts
+      = createPathPoints(current_pos, previous_path_x, previous_path_y);
   vector<double> spline_pts_x = spline_pts[0];
   vector<double> spline_pts_y = spline_pts[1];
   fillPathPoints(spline_pts_x, spline_pts_y, planned_pos,
@@ -119,16 +109,18 @@ vector<double> PathPlanner::getPlannedPosition(
     return current_pos;
   }
 
-  const double prev_pos_x = prev_path_size < 2 ? current_pos[X_IDX] : previous_path_x[prev_path_size - 2];
-  const double prev_pos_y = prev_path_size < 2 ? current_pos[Y_IDX] : previous_path_y[prev_path_size - 2];
+  const double prev_pos_x
+      = prev_path_size < 2 ? current_pos[X_IDX] : previous_path_x[prev_path_size - 2];
+  const double prev_pos_y
+      = prev_path_size < 2 ? current_pos[Y_IDX] : previous_path_y[prev_path_size - 2];
   const double planned_pos_x = previous_path_x[prev_path_size - 1];
   const double planned_pos_y = previous_path_y[prev_path_size - 1];
   const double planned_yaw
       = tools.normalizeAngle(atan2(planned_pos_y - prev_pos_y, planned_pos_x - prev_pos_x));
   const double planned_speed
       = tools.distance(prev_pos_x, prev_pos_y, planned_pos_x, planned_pos_y) / move_time;
-  const vector<double> planned_pos_sd
-      = tools.getFrenet(planned_pos_x, planned_pos_y, planned_yaw, map_waypoints_x, map_waypoints_y);
+  const vector<double> planned_pos_sd = tools.getFrenet(planned_pos_x, planned_pos_y, planned_yaw,
+                                                        map_waypoints_x, map_waypoints_y);
   return {planned_pos_x, planned_pos_y, planned_yaw, planned_speed,
           planned_pos_sd[X_IDX], planned_pos_sd[Y_IDX]};
 }
@@ -178,7 +170,8 @@ void PathPlanner::fillPathPoints(
     const vector<double> &map_waypoints_s) {
   for (int i = 0; i < 5; i++) {
     const vector<double> wp = tools.getXY(
-        planned_pos[S_IDX] + (i+1) * max(planned_pos[SPEED_IDX] * action_seconds, MIN_ACTION_METERS),
+        planned_pos[S_IDX] + (i+1) * max(planned_pos[SPEED_IDX] * action_seconds,
+                                         MIN_ACTION_METERS),
         lane * 4.0 + 2.0,
         map_waypoints_s, map_waypoints_x, map_waypoints_y);
     spline_pts_x.push_back(wp[0]);
@@ -198,7 +191,8 @@ void PathPlanner::addNextPoints(
 
   for (int i = 0; i < spline_pts_x.size(); i++) {
     // Moving points to (0, 0) with angle 0
-    tools.translateAndRotate(spline_pts_x[i], spline_pts_y[i], planned_pos_x, planned_pos_y, planned_yaw);
+    tools.translateAndRotate(spline_pts_x[i], spline_pts_y[i],
+                             planned_pos_x, planned_pos_y, planned_yaw);
   }
 
   tk::spline spline;
@@ -221,7 +215,7 @@ void PathPlanner::addNextPoints(
 ScanStatus PathPlanner::scanSurroundings(
     const std::vector<double> &current_pos,
     const double &time_in_future, const std::vector<double> &planned_pos,
-    const vector<vector<double>> &sensor_fusion) {
+    vector<vector<double>> &sensor_fusion) {
   const double current_pos_s = current_pos[S_IDX];
   const double current_speed = current_pos[SPEED_IDX];
   const double planned_pos_s = planned_pos[S_IDX];
@@ -229,49 +223,64 @@ ScanStatus PathPlanner::scanSurroundings(
 
   ScanStatus scan_status;
   scan_status.carInFrontIsTooClose = false;
-  scan_status.carInFrontIsDangerouslyClose = false;
   scan_status.gapAtLeftLane = true;
   scan_status.gapAtRightLane = true;
+  scan_status.carAheadInFront = NULL;
+  scan_status.carAheadAtLeftLane = NULL;
+  scan_status.carAheadAtRightLane = NULL;
 
   for (int i = 0; i < sensor_fusion.size(); i++) {
-    const double checked_car_vx = sensor_fusion[i][3];
-    const double checked_car_vy = sensor_fusion[i][4];
-    const double checked_car_speed = sqrt(checked_car_vx * checked_car_vx + checked_car_vy * checked_car_vy);
-    double checked_car_s = sensor_fusion[i][5];
-    const double checked_car_d = sensor_fusion[i][6];
+    const double checked_car_vx = sensor_fusion[i][SENSOR_FUSION_VX_IDX];
+    const double checked_car_vy = sensor_fusion[i][SENSOR_FUSION_VY_IDX];
+    const double checked_car_speed
+        = sqrt(checked_car_vx * checked_car_vx + checked_car_vy * checked_car_vy);
+    double checked_car_s = sensor_fusion[i][SENSOR_FUSION_S_IDX];
+    const double checked_car_d = sensor_fusion[i][SENSOR_FUSION_D_IDX];
 
     if (isCarInLane(checked_car_d, lane) &&
-        checked_car_s >= current_pos_s &&
-        checked_car_s < current_pos_s + current_speed * security_seconds_ahead * 0.75) {
-      scan_status.carInFrontIsDangerouslyClose = true;
+        isCarInFrontTooClose(current_pos_s, current_speed, checked_car_s,
+                             security_reaction_rate)) {
+      scan_status.carInFrontIsTooClose = true;
     }
 
     checked_car_s += checked_car_speed * time_in_future;
 
-    if (isCarInLane(checked_car_d, lane) && isCarToClose(planned_pos_s, planned_speed, checked_car_s)) {
+    if (isCarInLane(checked_car_d, lane) &&
+        isCarInFrontTooClose(planned_pos_s, planned_speed, checked_car_s)) {
       scan_status.carInFrontIsTooClose = true;
     }
 
     if (lane > FIRST_LANE) {
-      if (isCarInLane(checked_car_d, lane - 1) &&
-          (isCarToClose(planned_pos_s, planned_speed, checked_car_s) ||
-           blocksLaneChange(planned_pos_s, planned_speed, checked_car_s, checked_car_speed))) {
-           //isCarToClose(checked_car_s, checked_car_speed, planned_pos_s))) {
+      if (doesCarBlockLaneChange(planned_pos_s, planned_speed, checked_car_s,
+                                 checked_car_d, checked_car_speed, lane - 1)) {
         scan_status.gapAtLeftLane = false;
-      }
+      };
     } else {
       scan_status.gapAtLeftLane = false;
     }
 
     if (lane < LAST_LANE) {
-      if (isCarInLane(checked_car_d, lane + 1) &&
-          (isCarToClose(planned_pos_s, planned_speed, checked_car_s) ||
-           //isCarToClose(checked_car_s, checked_car_speed, planned_pos_s))) {
-           blocksLaneChange(planned_pos_s, planned_speed, checked_car_s, checked_car_speed))) {
+      if (doesCarBlockLaneChange(planned_pos_s, planned_speed, checked_car_s,
+                                 checked_car_d, checked_car_speed, lane + 1)) {
         scan_status.gapAtRightLane = false;
       }
     } else {
       scan_status.gapAtRightLane = false;
+    }
+
+    if (isClosestCarAheadInLane(scan_status.carAheadInFront, lane,
+                                checked_car_s, checked_car_d, planned_pos_s)) {
+      scan_status.carAheadInFront = &sensor_fusion[i];
+    }
+    if (lane > FIRST_LANE &&
+        isClosestCarAheadInLane(scan_status.carAheadAtLeftLane, lane - 1,
+                                checked_car_s, checked_car_d, planned_pos_s)) {
+      scan_status.carAheadAtLeftLane = &sensor_fusion[i];
+    }
+    if (lane < LAST_LANE &&
+        isClosestCarAheadInLane(scan_status.carAheadAtRightLane, lane + 1,
+                                checked_car_s, checked_car_d, planned_pos_s)) {
+      scan_status.carAheadAtRightLane = &sensor_fusion[i];
     }
   }
 
@@ -279,10 +288,6 @@ ScanStatus PathPlanner::scanSurroundings(
 }
 
 State PathPlanner::getNextState(const ScanStatus &scan_status) {
-  if (scan_status.carInFrontIsDangerouslyClose) {
-    return EMERGENCY_BREAK;
-  }
-
   if (scan_status.carInFrontIsTooClose) {
     if (scan_status.gapAtLeftLane) {
       return CHANGE_TO_LEFT_LANE;
@@ -304,9 +309,6 @@ State PathPlanner::getNextState(const ScanStatus &scan_status) {
 }
 
 double PathPlanner::adjustSpeed(const double &speed, const State &state) {
-  if (state == EMERGENCY_BREAK) {
-    return max(speed - ideal_acceleration * move_time, 0.0);
-  }
   if (state == REDUCE_SPEED) {
     return max(speed - ideal_acceleration * move_time, 0.0);
   }
@@ -326,22 +328,58 @@ bool PathPlanner::isCarInLane(const double &car_d, const int &lane) {
   return false;
 }
 
-bool PathPlanner::isCarToClose(const double &from_car_s, const double &from_car_speed,
-                               const double &to_car_s) {
-  if (from_car_s <= to_car_s && to_car_s < from_car_s + from_car_speed * security_seconds_ahead) {
+bool PathPlanner::isCarInFrontTooClose(
+    const double &from_car_s, const double &from_car_speed, const double &target_car_s) {
+  return isCarInFrontTooClose(from_car_s, from_car_speed, target_car_s, 1.0);
+}
+
+bool PathPlanner::isCarInFrontTooClose(
+    const double &from_car_s, const double &from_car_speed, const double &target_car_s,
+    const double &security_seconds_ahead_proportion) {
+  if (from_car_s <= target_car_s &&
+      target_car_s <
+      from_car_s + from_car_speed * security_seconds_ahead * security_seconds_ahead_proportion) {
     return true;
   }
   return false;
 }
 
-bool PathPlanner::blocksLaneChange(const double &car_s, const double &car_speed,
-                                   const double &target_car_s, const double &target_car_speed) {
-  if (target_car_s <= car_s && car_s < target_car_s + target_car_speed * security_seconds_ahead) {
-    if (target_car_speed < car_speed &&
-        car_s > target_car_s +
-                target_car_speed * security_seconds_ahead * (1.0 - takeover_agressivity_rate)) {
-       return false;
-    }
+bool PathPlanner::doesCarBlockLaneChange(
+    const double &car_s, const double &car_speed,
+    const double &target_car_s, const double &target_car_d, const double &target_car_speed,
+    const int &target_lane) {
+  if (!isCarInLane(target_car_d, target_lane)) {
+    return false;
+  }
+  if (isCarInFrontTooClose(car_s, car_speed, target_car_s)) {
+    return true;
+  }
+  if (doesCarBehindBlockLaneChange(car_s, car_speed, target_car_s, target_car_speed)) {
+    return true;
+  }
+  return false;
+}
+
+bool PathPlanner::doesCarBehindBlockLaneChange(
+    const double &car_s, const double &car_speed,
+    const double &target_car_s, const double &target_car_speed) {
+  if (!isCarInFrontTooClose(target_car_s, target_car_speed, car_s)) {
+    return false;
+  }
+  if (target_car_speed < car_speed &&
+      !isCarInFrontTooClose(target_car_s, target_car_speed, car_s,
+                            1.0 - takeover_agressivity_rate)) {
+    return false;
+  }
+  return true;
+}
+
+bool PathPlanner::isClosestCarAheadInLane(
+    vector<double> *&tracked_car, const double target_lane,
+    const double checked_car_s, const double checked_car_d, const double planned_pos_s) {
+  if (isCarInLane(checked_car_d, target_lane) &&
+      checked_car_s > planned_pos_s &&
+      (tracked_car == NULL || checked_car_s < tracked_car->at(SENSOR_FUSION_S_IDX))) {
     return true;
   }
   return false;
--- src/path_planner.h
+++ src/path_planner.h
@@ -7,17 +7,18 @@
 
 struct ScanStatus {
     bool carInFrontIsTooClose;
-    bool carInFrontIsDangerouslyClose;
     bool gapAtLeftLane;
     bool gapAtRightLane;
+    std::vector<double> *carAheadInFront;
+    std::vector<double> *carAheadAtLeftLane;
+    std::vector<double> *carAheadAtRightLane;
 };
 
 enum State {
   ADVANCE,
   REDUCE_SPEED,
   CHANGE_TO_LEFT_LANE,
-  CHANGE_TO_RIGHT_LANE,
-  EMERGENCY_BREAK
+  CHANGE_TO_RIGHT_LANE
 };
 
 class PathPlanner {
@@ -30,8 +31,9 @@ class PathPlanner {
       const double &max_acceleration,
       const double &security_seconds_ahead,
       const double &planning_seconds_ahead,
+      const double &action_seconds,
       const double &takeover_agressivity_rate,
-      const double &action_seconds);
+      const double &security_reaction_rate);
 
     std::vector<std::vector<double>> updatePath(
       const nlohmann::json &car_data,
@@ -53,6 +55,14 @@ class PathPlanner {
     const int S_IDX = 4;
     const int D_IDX = 5;
 
+    const int SENSOR_FUSION_ID_IDX = 0;
+    const int SENSOR_FUSION_X_IDX = 1;
+    const int SENSOR_FUSION_Y_IDX = 2;
+    const int SENSOR_FUSION_VX_IDX = 3;
+    const int SENSOR_FUSION_VY_IDX = 4;
+    const int SENSOR_FUSION_S_IDX = 5;
+    const int SENSOR_FUSION_D_IDX = 6;
+
     Tools tools;
 
     double max_speed_mph;
@@ -60,8 +70,9 @@ class PathPlanner {
     double max_acceleration;
     double security_seconds_ahead;
     double planning_seconds_ahead;
-    double takeover_agressivity_rate; // 0.0 to 1.0
     double action_seconds;
+    double takeover_agressivity_rate; // 0.0 to 1.0
+    double security_reaction_rate; // 0.0 to 1.0
 
     double ideal_speed; // Desired speed in m/s
     double ideal_acceleration;
@@ -72,49 +83,92 @@ class PathPlanner {
 
     void initialize();
 
-    std::vector<double> getCurrentPosition(const nlohmann::json &car_data);
+    std::vector<double> getCurrentPosition(
+        const nlohmann::json &car_data);
 
     std::vector<double> getPlannedPosition(
         const std::vector<double> &current_pos,
-        const std::vector<double> &ptr_previous_path_x, const std::vector<double> &ptr_previous_path_y,
-        const std::vector<double> &map_waypoints_x, const std::vector<double> &map_waypoints_y);
+        const std::vector<double> &ptr_previous_path_x,
+        const std::vector<double> &ptr_previous_path_y,
+        const std::vector<double> &map_waypoints_x,
+        const std::vector<double> &map_waypoints_y);
 
     std::vector<std::vector<double>> createPathPoints(
         const std::vector<double> &current_pos,
-        const std::vector<double> &previous_path_x, const std::vector<double> &previous_path_y);
+        const std::vector<double> &previous_path_x,
+        const std::vector<double> &previous_path_y);
 
     void movePreviousPointsToNext(
-      const std::vector<double> &previous_path_x, const std::vector<double> &previous_path_y,
-      std::vector<double> &next_x, std::vector<double> &next_y);
+        const std::vector<double> &previous_path_x,
+        const std::vector<double> &previous_path_y,
+        std::vector<double> &next_x,
+        std::vector<double> &next_y);
 
     void fillPathPoints(
-      std::vector<double> &spline_pts_x, std::vector<double> &spline_pts_y,
-      const std::vector<double> &planned_pos,
-      const std::vector<double> &map_waypoints_x,
-      const std::vector<double> &map_waypoints_y,
-      const std::vector<double> &map_waypoints_s);
+        std::vector<double> &spline_pts_x,
+        std::vector<double> &spline_pts_y,
+        const std::vector<double> &planned_pos,
+        const std::vector<double> &map_waypoints_x,
+        const std::vector<double> &map_waypoints_y,
+        const std::vector<double> &map_waypoints_s);
 
     void addNextPoints(
-        const std::vector<double> &planned_pos, const State &state,
-        std::vector<double> &spline_pts_x, std::vector<double> &spline_pts_y,
-        std::vector<double> &next_x, std::vector<double> &next_y,
+        const std::vector<double> &planned_pos,
+        const State &state,
+        std::vector<double> &spline_pts_x,
+        std::vector<double> &spline_pts_y,
+        std::vector<double> &next_x,
+        std::vector<double> &next_y,
         const int &prev_path_size);
 
     ScanStatus scanSurroundings(
         const std::vector<double> &current_pos,
-        const double &time_in_future, const std::vector<double> &future_pos,
-        const std::vector<std::vector<double>> &sensor_fusion);
+        const double &time_in_future,
+        const std::vector<double> &future_pos,
+        std::vector<std::vector<double>> &sensor_fusion);
 
-    State getNextState(const ScanStatus &scan_status);
+    State getNextState(
+        const ScanStatus &scan_status);
 
-    double adjustSpeed(const double &speed, const State &state);
+    double adjustSpeed(
+        const double &speed,
+        const State &state);
 
-    bool isCarInLane(const double &car_d, const int &lane);
+    bool isCarInLane(
+        const double &car_d,
+        const int &lane);
 
-    bool isCarToClose(const double &from_car_s, const double &from_car_speed, const double &to_car_s);
+    bool isCarInFrontTooClose(
+        const double &from_car_s,
+        const double &from_car_speed,
+        const double &target_car_s);
 
-    bool blocksLaneChange(const double &from_car_s, const double &from_car_speed,
-                          const double &target_car_s, const double &target_car_speed);
+    bool isCarInFrontTooClose(
+        const double &from_car_s,
+        const double &from_car_speed,
+        const double &target_car_s,
+        const double &security_seconds_ahead_proportion);
+
+    bool doesCarBlockLaneChange(
+        const double &from_car_s,
+        const double &from_car_speed,
+        const double &target_car_s,
+        const double &target_car_d,
+        const double &target_car_speed,
+        const int &target_lane);
+
+    bool doesCarBehindBlockLaneChange(
+        const double &from_car_s,
+        const double &from_car_speed,
+        const double &target_car_s,
+        const double &target_car_speed);
+
+    bool isClosestCarAheadInLane(
+        std::vector<double> *&tracked_car,
+        const double target_lane,
+        const double checked_car_s,
+        const double checked_car_d,
+        const double planned_pos_s);
 };
 
 #endif /* PATH_PLANNER_H */
